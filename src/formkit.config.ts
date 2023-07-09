0df03a59d082c6640fcf80b1754fa2c72df11f07
darwishdev <a.darwish.dev@gmail.com>
2023-07-09 10:16:45 +0300

    load formkit token from env

---
+++ src/formkit.config.ts
@@ -0,0 +1,16 @@
+import "@formkit/themes/genesis"
+import { genesisIcons } from "@formkit/icons"
+import type { DefaultConfigOptions } from '@formkit/vue'
+import { createProPlugin, inputs } from '@formkit/pro'
+import '@formkit/pro/genesis'
+// import SubmitLoading from '../inputs/SubmitLoading.vue'
+
+const pro = createProPlugin(import.meta.env.VITE_FORMKIT_TOKEN, inputs)
+
+const config: DefaultConfigOptions = {
+    plugins: [pro],
+    icons: { ...genesisIcons },
+
+}
+
+export default config

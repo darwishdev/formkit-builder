79cbb0d4d26e089d400c259bb1191d2144748637
darwishdev <a.darwish.dev@gmail.com>
2023-07-09 10:45:07 +0300

    add primevue toast

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import Toast from 'primevue/toast';
 </script>
 
 <template>
@@ -13,4 +14,15 @@ import { RouterLink, RouterView } from 'vue-router'
   </header>
 
   <RouterView />
+  <toast>
+    <template #message="slotProps">
+      <div class="flex toast-inner flex-column align-items-center" style="flex: 1">
+        <div class="text-center">
+          <h1 class="font-bold text-4xl ">{{ slotProps.message.summary }}</h1>
+          <p class="my-1">{{ slotProps.message.detail }}</p>
+        </div>
+
+      </div>
+    </template>
+  </toast>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import formKitConfig from './formkit.config'
 import { plugin, defaultConfig } from '@formkit/vue'
+import ToastService from 'primevue/toastservice';
 import router from './router'
-
+import { useToast } from "primevue/usetoast";
+import FormkitBuilder from "./FormkitBuilder"
 const app = createApp(App)
 
-app.use(plugin, defaultConfig(formKitConfig)).use(router)
+
+app.use(FormkitBuilder, { router, useToast }).use(ToastService).use(plugin, defaultConfig(formKitConfig)).use(router)
 
 app.mount('#app')
